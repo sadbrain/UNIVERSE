5227b6672782883a5533aa3ea608631b50ff0fe4
KhanhNhi <nhi.tuong25@student.passerellesnumeriques.org>
2023-12-30 16:30:59 +0700

    Nhi-feat/USP-5/FE-Selling_Products

--- area/customer/views/Home/index.php
+++ area/customer/views/Home/index.php
@@ -64,3 +64,55 @@
   </div>
 
 
+  <div class="product_main mb-5">
+        <?php
+            foreach($products as $obj){
+                $product = $obj['Product'];
+                $discount = $obj['Discount'];
+                $product_inventory = $obj['ProductInventory'];
+        ?>
+            <div class="product_info">
+                <img
+                src="<?=URL_ROOT.URL_SUBFOLDER. '/'.$product -> get_thumbnail()?>"
+                alt="Fashion Product Image"/>
+                <div class="p-3">
+                        <a href="javascript:void(0)"
+                        ><h5><b><?=$product -> get_name()?></b></h5></a
+                    >
+                    <h6 class="brand"><b><?=$product -> get_brand()?></b></h6>
+                        <p class="price-container">
+                            <?php
+                                $current_date = new DateTime();
+                                $discount_to = $discount -> get_discount_to();
+                                if($current_date <= $discount_to){
+                                    $price = $product -> get_price();
+                                    $discount_price = ($price-$price * $discount-> get_discount_price()/100);
+                                    echo "  <span class='discounted-price'>".$discount_price."</span>
+                                            <span class='unit'>VND</span>
+                                            <span class='original-price'>".$price."</span>
+                                            <span class='unit'>VND</span>";
+                                }else{
+                                    echo"<span class=''>".$product -> get_price()."</span>
+                                    <span class='unit'>VND</span>";
+                                }
+                            ?>
+
+                        </p>
+                    <div class="product_action">
+                        <div class="star_rating">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        </div>
+                        <h6 class="quantity"><?= $product_inventory -> get_quantity_buyed() . " sold"?></h6>
+                    </div>
+                </div>
+            </div>
+        <?php }?>    
+        
+
+    
+    </div>
+
