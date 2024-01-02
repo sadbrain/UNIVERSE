8fd31b3b723369c5f687af22d6a596a8bed88935
KhanhNhi <nhi.tuong25@student.passerellesnumeriques.org>
2024-01-02 11:03:32 +0700

    Nhi-feat/Payment_Contact

---
+++ area/customer/views/Checkout/index.php
@@ -0,0 +1,16 @@
+<?= load_css("\wwwroot\customer\css\pages\Checkout\index.css") ?>
+
+<div class="flex-container">
+    <div class="box">
+        <p> Contact </p>
+        <input type="text" id="fname" name="fname" placeholder="Full name"><br><br>
+        <input type="text" id="fname" name="fname" placeholder="Address"><br><br>
+        <input type="text" id="fname" name="fname" placeholder="Phone number "><br><br>
+        <input type="text" id="fname" name="fname" placeholder="Address email"><br><br>
+
+    </div>
+    <div class="box">
+        
+    </div>
+
+</div>
